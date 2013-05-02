66112fa0e9dbc6de02311870847b010eaaa92cb7
Benjamin Fisher <neohell@hotmail.com>
2013-05-02 17:58:14 -0400

    Added images assets and started coding start screen

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 Breakout
 ========
 
-Built with [iio](http://iioengine.com) Engine
----------------------
+Built with [iio](http://iioengine.com) Engine on top of Express
+---------------------------------------------------------------
 
 ...for the [Breakouts](http://city41.github.io/breakouts/index.html) project
--- public/javascripts/iioBreakout.js
+++ public/javascripts/iioBreakout.js
@@ -3,6 +3,28 @@
 // Site: http://benjaminjfisher.com
 // Date: May 2013
 
-window.onload = function(io){
-	console.log(io);
+var breakout = function(io){
+	// start the debugger
+	// io.activateDebugger();
+
+	io.addGroup('background', 0);
+	io.addGroup('foreground', 10);
+
+
+	var bg = new iio.ioRect(io.canvas.center)
+		.createWithImage('images/bg_prerendered.png', function(){
+			io.addObj(bg)
+		});
+
+	var startText = new iio.ioText('Click to start', io.canvas.center)
+			.setFont("24px Arial")
+			.setTextAlign('center')
+			.setFillStyle('black');
+
+	io.addObj(startText)
+
+	io.addToGroup('background', bg);
+	io.addToGroup('foreground', startText);
 };
+
+iio.start(breakout, 'canvas');
